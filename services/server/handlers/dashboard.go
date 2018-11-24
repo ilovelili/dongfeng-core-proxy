7c82baf2da40370183bfcc6358acdecd850de248
ilovelili <route666@live.cn>
2018-11-24 23:24:45 +0900

    add dashboard api

---
+++ services/server/handlers/dashboard.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	restful "github.com/emicklei/go-restful"
+	"github.com/ilovelili/dongfeng-core-proxy/services/utils"
+	errorcode "github.com/ilovelili/dongfeng-error-code"
+	protobuf "github.com/ilovelili/dongfeng-protobuf"
+)
+
+// Dashboard dashboard
+func Dashboard(req *restful.Request, rsp *restful.Response) {
+	idtoken, _ := utils.ResolveIDToken(req)
+	response, err := newclient().Dashboard(ctx(req), &protobuf.DashboardRequest{Token: idtoken})
+	if err != nil {
+		writeError(rsp, errorcode.Pipe, err.Error())
+	} else {
+		rsp.WriteAsJson(response)
+	}
+}
